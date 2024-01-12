24f64bbcff304635b9de4082f97c68dd98215578
guilhermebernar <guilhermebernardo@id.uff.br>
2024-01-12 17:19:42 -0300

    Updates

---
+++ assets/Newsletter-49gjFFcA.js
@@ -0,0 +1,77 @@
+import{u as i,j as t}from"./index-oaQE-qEQ.js";const o=i.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  background: #FAFAFA;
+  border-top: 2px solid #e4e4e4 !important;
+  border-bottom: 2px solid #e4e4e4 !important;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 40px 0 !important;
+  }
+`,e=i.h2`
+    color: #353535 !important;
+    text-align: center !important;
+    font-family: Titillium Web !important;
+    font-size: 24px !important;
+    font-style: normal !important;
+    font-weight: 700 !important;
+    margin: auto 0 !important;
+    @media (max-width: 768px) {
+      text-align: center !important;
+      margin: 24px 0 12px 0 !important;
+    }
+`,n=i.form`
+position: relative;
+  display: flex;
+  flex-direction: row !important;
+  width: 80%;
+  justify-content: space-between;
+  margin: 8px auto 24px auto !important;
+  
+  @media (min-width: 768px) {
+    width: 50%;
+    flex-direction: row !important;
+    width: fit-content;
+    align-items: center;
+    margin: 0 0 0 20px !important;
+
+  }
+`,r=i.input`
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+    width: 60%;
+    height: 20px;
+
+  @media (min-width: 768px) {
+    margin-right: 10px;
+    margin-bottom: 0;
+    width: 468px;
+  }
+`,a=i.button`
+  padding: auto;
+  background-color: #ffa500; // Cor do botão
+  border: none;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
+  width: 79px;
+    height: 40px;
+    text-align: center;
+    padding: auto;
+
+    @media (min-width: 768px) {
+        position: absolute;
+        right: 0;
+    }
+
+  &:hover {
+    background-color: #e69500; // Cor do botão ao passar o mouse
+  }
+`,d=()=>t.jsxs(o,{children:[t.jsx(e,{children:"Receba Nossa Newsletter"}),t.jsxs(n,{children:[t.jsx(r,{type:"email",placeholder:"Digite seu e-mail"}),t.jsx(a,{type:"submit",children:"Enviar"})]})]});export{d as default};
---
+++ assets/PromotionalBlock-PcUnv_mT.js
@@ -0,0 +1,44 @@
+import{u as t,j as i}from"./index-oaQE-qEQ.js";const o="/assets/banner-cta-oUPgsH0t.png",n=t.div`
+    display: flex;
+    flex-direction: column;
+    margin: 20px 10% !important;
+  @media (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin: 80px 5% !important;
+  } 
+`,e=t.div`
+    width: 100%;
+    @media (min-width: 768px) {
+        width: 30%;
+    }
+`,a=t.div`
+
+  img {
+    max-width: 100%;
+    @media (min-width: 768px) {
+        width: 1120px !important;
+    }
+  }
+`,m=t.h2`
+    color: #000;
+    font-family: Titillium Web !important;
+    font-size: 24px !important;
+    font-style: normal !important;
+    font-weight: 700 !important;
+    line-height: normal !important;
+    margin: 23px 0 18px 0 !important;
+    @media (min-width: 768px) {
+    }
+`,r=t.p`
+    color:#000 !important;
+    width: 88%;
+    font-family: Titillium Web !important;
+    font-size: 14px !important;
+    font-style: normal !important;
+    font-weight: 400 !important;
+    line-height: 18px !important;
+    margin: 0 0 20px 0 !important;
+`,l=()=>i.jsxs(n,{children:[i.jsxs(e,{children:[i.jsx(m,{children:"Lorem ipsum"}),i.jsx(r,{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip. Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."})]}),i.jsx(a,{children:i.jsx("img",{src:o,alt:"Nova Coleção de outono-inverno"})})]});export{l as default};
